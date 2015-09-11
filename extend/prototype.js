472b219ebaf01b7c3c501974c3e895f46e39dd75
roychen <roychad.cy7@gmail.com>
2015-09-11 18:11:09 +0800

    prototype inherit diff

--- extend/prototype.js
+++ extend/prototype.js
@@ -1,13 +1,35 @@
 // Prototype
-var superClass = function() {};
-superClass.prototype.func1 = function() {console.log(1);}
-superClass.prototype.func2 = function() {console.log(2);}
+// Object Create
+console.log("Object Create===============");
+gen(function(superClass, subClass) {
+    subClass.prototype = Object.create(superClass.prototype);
+});
 
-var subClass = function() {};
-subClass.prototype = Object.create(superClass.prototype);
-subClass.prototype.func1 = function() {console.log(11);}
+// New
+console.log("New===============");
+gen(function(superClass, subClass) {
+    subClass.prototype = new superClass;
+});
 
-var ins = new subClass();
+function gen (inheritFunc) {
+    // Define super Class
+    var superClass = function() { this.super = 'super'; };
+    superClass.prototype.func1 = function() {console.log(1);}
+    superClass.prototype.func2 = function() {console.log(2);}
+    superClass.prototype.attrSuper = 'attrSuper';
 
-ins.func1();
-ins.func2();
+    // Define sub Class
+    var subClass = function() { this.sub = 'sub' };
+    subClass.prototype.func1 = function() {console.log(11);}
+    inheritFunc(superClass, subClass);
+    
+    // Tester
+    var ins = new subClass();
+
+    ins.func1();
+    ins.func2();
+    
+    console.log('attr super: ' + ins.attrSuper);
+    console.log('super: ' + ins.super);
+    console.log('sub: ' + ins.sub);
+}
